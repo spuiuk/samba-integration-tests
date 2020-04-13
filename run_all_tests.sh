fddad656ec176efc0b776ea572e0d06c7b1a83ca
Sachin Prabhu <sprabhu@redhat.com>
2020-04-13 01:02:11 +0100

    Use a file to specify the tests to run
    This helps us control the order in which to run the tests.
    
    Signed-off-by: Sachin Prabhu <sprabhu@redhat.com>

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,9 +4,9 @@ test_info_file_dir=$(cd `dirname $1` && pwd)
 test_info_file=$test_info_file_dir/`basename $1`
 
 cd testcases
-for i in */run_test
+for i in `cat tests`
 do
-	if [ -x $i ] && ! $i $test_info_file
+	if [ -x $i/run_test ] && ! $i/run_test $test_info_file
 	then
 		exit 1
 	fi
